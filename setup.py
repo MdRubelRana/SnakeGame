f2532edcd5f40f5c7ebdc93bc40104f4e7fb7204
Md Rubel Rana <rubel.rana@northsouth.edu>
2021-04-06 14:29:52 +0600

    first commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+from cx_Freeze import *
+import sys
+includefiles=['assets/icon.ico','assets/head.gif','assets/tail.gif','assets/apple.gif']
+base=None
+if sys.platform=="win32":
+    base="Win32GUI"
+
+shortcut_table=[
+    ("DesktopShortcut",
+     "DesktopFolder",
+     "SnakeGame",
+     "TARGETDIR",
+     "[TARGETDIR]\snake.exe",
+     None,
+     None,
+     None,
+     None,
+     None,
+     None,
+     "TARGETDIR",
+     )
+]
+msi_data={"Shortcut":shortcut_table}
+
+bdist_msi_options={'data':msi_data}
+setup(
+    version="0.1",
+    description="Snake Game",
+    author="Md. Rubel Rana",
+    name="Snake Game",
+    options={'build_exe':{'include_files':includefiles},'bdist_msi':bdist_msi_options,},
+    executables=[
+        Executable(
+            script="snake.py",
+            base=base,
+            icon='assets/icon.ico',
+        )
+    ]
+)
